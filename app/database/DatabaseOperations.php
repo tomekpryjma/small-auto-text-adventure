299c07b27218f2c6efc1ce452954e3ef9230f053
Tomek <tompe97@tlen.pl>
2020-10-15 23:13:00 +0100

    Add insert & delete operations

--- app/database/DatabaseOperations.php
+++ app/database/DatabaseOperations.php
@@ -10,4 +10,27 @@ class DatabaseOperations
     {
         $this->connection = $databaseInstance->getConnection();
     }
+
+    public function insertInto($table, $values): bool
+    {
+        $placeholders = [];
+
+        for ($index = 0; $index < count($values); $index++) {
+            $placeholders[] = '?';
+        }
+
+        $sql = 'INSERT INTO '. $table .'  (' . implode(',', array_keys($values)) . ')';
+        $sql .=  ' VALUES ('. implode(',', $placeholders) .')';
+
+        $statement = $this->connection->prepare($sql);
+        return $statement->execute(array_values($values));
+    }
+
+    public function deleteFrom($table, $key, $value): bool
+    {
+        $sql = "DELETE FROM $table WHERE $key = ?";
+
+        $statement = $this->connection->prepare($sql);
+        return $statement->execute([$value]);
+    }
 }
