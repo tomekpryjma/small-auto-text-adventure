6a15ff6a64ead376da6ecfc917932c221899d909
Tomek <tompe97@tlen.pl>
2020-10-19 21:44:09 +0100

    Changes according to PHPCS

--- app/tests/DatabaseOperationsTest.php
+++ app/tests/DatabaseOperationsTest.php
@@ -8,11 +8,17 @@ use App\Database\DatabaseOperations;
 
 class DatabaseOperationsTest extends TestCase
 {
-    private $defaultTable = 'characters';
-    private $defaultColumn = 'name';
-    private $defaultColumnValue = '__this_is_a_tester__';
-    private $defaultColumnValueAfterUpdate = '__updated_tester__';
+    private $_defaultTable = 'characters';
+    private $_defaultColumn = 'name';
+    private $_defaultColumnValue = '__this_is_a_tester__';
+    private $_defaultColumnValueAfterUpdate = '__updated_tester__';
 
+    /**
+     * Test to determine whether the program can insert a row into
+     * the specified table.
+     * 
+     * @return void
+     */
     public function testCanInsertIntoDatabase()
     {
         $database = new Database();
@@ -21,70 +27,94 @@ class DatabaseOperationsTest extends TestCase
             'alive' => true,
             'longest_streak' => 2
         ];
-        $schema[$this->defaultColumn] = $this->defaultColumnValue;
-        $this->assertTrue($connection->insert($this->defaultTable, $schema));
+        $schema[$this->_defaultColumn] = $this->_defaultColumnValue;
+        $this->assertTrue($connection->insert($this->_defaultTable, $schema));
     }
 
-    public function testCanGetAllFromColumn()
+    /**
+     * Test to determine whether the program can get all columns of
+     * a row from a specified table.
+     * 
+     * @return void
+     */
+    public function testCanGetAllFromRow()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
         $this->assertIsArray(
             $connection->get(
-                $this->defaultTable,
-                $this->defaultColumn,
-                $this->defaultColumnValue
+                $this->_defaultTable,
+                $this->_defaultColumn,
+                $this->_defaultColumnValue
             )
         );
     }
 
+    /**
+     * Test to determine whether the program can get the value of
+     * a specific column from a table.
+     * 
+     * @return void
+     */
     public function testCanGetSpecificColumn()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
         $this->assertEquals(
-            $this->defaultColumnValue, $connection->get(
-                $this->defaultTable,
-                $this->defaultColumn,
-                $this->defaultColumnValue,
-                $this->defaultColumn
+            $this->_defaultColumnValue, $connection->get(
+                $this->_defaultTable,
+                $this->_defaultColumn,
+                $this->_defaultColumnValue,
+                $this->_defaultColumn
             )
         );
     }
 
+    /**
+     * Test to determine whether the program can update a column of
+     * a row from a specified table.
+     * 
+     * @return void
+     */
     public function testCanUpdateInDatabase()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
         $this->assertTrue(
             $connection->update(
-                $this->defaultTable,
-                $this->defaultColumn,
-                $this->defaultColumnValue,
-                $this->defaultColumn,
-                $this->defaultColumnValueAfterUpdate
+                $this->_defaultTable,
+                $this->_defaultColumn,
+                $this->_defaultColumnValue,
+                $this->_defaultColumn,
+                $this->_defaultColumnValueAfterUpdate
             )
         );
 
         $actualValue = $connection->get(
-            $this->defaultTable,
-            $this->defaultColumn,
-            $this->defaultColumnValueAfterUpdate,
-            $this->defaultColumn
+            $this->_defaultTable,
+            $this->_defaultColumn,
+            $this->_defaultColumnValueAfterUpdate,
+            $this->_defaultColumn
         );
 
-        $this->assertEquals($this->defaultColumnValueAfterUpdate, $actualValue);
+        $this->assertEquals($this->_defaultColumnValueAfterUpdate, $actualValue);
     }
 
+    /**
+     * Test to determine whether the program can delete a row from
+     * a specified column.
+     * 
+     * @return void
+     */
     public function testCanDeleteFromDatabase()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
         $this->assertTrue(
             $connection->delete(
-                $this->defaultTable,
-                $this->defaultColumn,
-                $this->defaultColumnValueAfterUpdate
+                $this->_defaultTable,
+                $this->_defaultColumn,
+                $this->_defaultColumnValueAfterUpdate
             )
         );
     }
