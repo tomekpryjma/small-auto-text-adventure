4d3178347ab0b29d2c4d5d8b29fb013289d7be4c
Tomek <tompe97@tlen.pl>
2020-10-20 00:20:04 +0100

    Add preliminary setup

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,5 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require 'config.php';
 
 use App\Database\Database;
+use App\Database\DatabaseOperations;
 
 $database = new Database();
+$connection = new DatabaseOperations($database);
