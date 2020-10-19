2c7f5bc2a5b99235755615334bb4e7c7087d5e63
Tomek <tompe97@tlen.pl>
2020-10-19 21:28:59 +0100

    Update tests with variables & add tests for update and get

--- app/tests/DatabaseOperationsTest.php
+++ app/tests/DatabaseOperationsTest.php
@@ -8,28 +8,76 @@ use App\Database\DatabaseOperations;
 
 class DatabaseOperationsTest extends TestCase
 {
+    private $defaultTable = 'characters';
+    private $defaultColumn = 'name';
+    private $defaultColumnValue = '__this_is_a_tester__';
+    private $defaultColumnValueAfterUpdate = '__updated_tester__';
+
     public function testCanInsertIntoDatabase()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
-        $this->assertTrue($connection->insertInto(
-            'characters',
-            [
-                'name' => 'Tester',
-                'alive' => true,
-                'longest_streak' => 2
-            ]
+        $schema = [
+            'alive' => true,
+            'longest_streak' => 2
+        ];
+        $schema[$this->defaultColumn] = $this->defaultColumnValue;
+        $this->assertTrue($connection->insert($this->defaultTable, $schema));
+    }
+
+    public function testCanGetAllFromColumn()
+    {
+        $database = new Database();
+        $connection = new DatabaseOperations($database->getInstance());
+        $this->assertIsArray($connection->get(
+            $this->defaultTable,
+            $this->defaultColumn,
+            $this->defaultColumnValue
         ));
+    }
+
+    public function testCanGetSpecificColumn()
+    {
+        $database = new Database();
+        $connection = new DatabaseOperations($database->getInstance());
+        $this->assertEquals($this->defaultColumnValue, $connection->get(
+            $this->defaultTable,
+            $this->defaultColumn,
+            $this->defaultColumnValue,
+            $this->defaultColumn
+        ));
+    }
+
+    public function testCanUpdateInDatabase()
+    {
+        $database = new Database();
+        $connection = new DatabaseOperations($database->getInstance());
+        $this->assertTrue($connection->update(
+            $this->defaultTable,
+            $this->defaultColumn,
+            $this->defaultColumnValue,
+            $this->defaultColumn,
+            $this->defaultColumnValueAfterUpdate
+        ));
+
+        $actualValue = $connection->get(
+            $this->defaultTable,
+            $this->defaultColumn,
+            $this->defaultColumnValueAfterUpdate,
+            $this->defaultColumn
+        );
+
+        $this->assertEquals($this->defaultColumnValueAfterUpdate, $actualValue);
     }
 
     public function testCanDeleteFromDatabase()
     {
         $database = new Database();
         $connection = new DatabaseOperations($database->getInstance());
-        $this->assertTrue($connection->deleteFrom(
-            'characters',
-            'name',
-            'Tester'
+        $this->assertTrue($connection->delete(
+            $this->defaultTable,
+            $this->defaultColumn,
+            $this->defaultColumnValueAfterUpdate
         ));
     }
 }
